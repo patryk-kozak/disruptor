b552adde474e5de679d187e5124cca60446f45fc
Patryk Kozak <thehackbee@gmail.com>
2022-05-11 11:19:57 +0200

    Change println

--- projects/bzl_ndn/jar_pack/JustMain.java
+++ projects/bzl_ndn/jar_pack/JustMain.java
@@ -3,6 +3,6 @@ package com.qarik.disruptor.bzl_ndn;
 
 public class JustMain {
     public static void main(String[] args) {
-        System.out.println("JustMain.app");
+        System.out.println("Totally ok code that is deterministic.");
     }
 }
