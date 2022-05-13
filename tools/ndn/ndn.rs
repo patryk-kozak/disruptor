25885f1ce897ec56a1bbbe1a05176c98706e2ba1
Patryk Kozak <thehackbee@gmail.com>
2022-05-13 11:24:36 +0200

    Another failure, rust based execlog parser exists already

--- tools/ndn/ndn.rs
+++ tools/ndn/ndn.rs
@@ -1,3 +1,24 @@
+use std::process::Command;
+
+fn exec_bazel(log_name: String) -> ! {
+    let exec = Command::new("bazel")
+        .arg("build")
+        .arg("--execution_log_json_file")
+        .arg(log_name)
+        .spawn()
+        .expect("bazel failed to start");
+
+    println!("status: {}", exec.status);
+    println!("stdout: {}", String::from_utf8_lossy(&exec.stdout));
+    println!("stderr: {}", String::from_utf8_lossy(&exec.stderr));
+}
+
+fn clean_bazel() {
+
+}
+
 fn main() {
-    println!("Hello world!");
+
+    exec_bazel("log_file_1.txt")
+
 }
--- tools/ndn/shell.nix
+++ tools/ndn/shell.nix
@@ -2,14 +2,23 @@
    system ? builtins.currentSystem
  , pkgs ? import <nixpkgs> { inherit system; }
 }:
-pkgs.mkShell {
+pkgs.mkShell rec {
   buildInputs = with pkgs; [
     pkgs.bazel_4
     pkgs.bazel-buildtools
     jdk11
     unixtools.util-linux
+    llvmPackages_latest.bintools
+    llvmPackages_latest.llvm
+    rustup
   ];
+
+  RUSTC_VERSION = pkgs.lib.readFile ./rust-toolchain;
+  LIBCLANG_PATH= pkgs.lib.makeLibraryPath [ pkgs.llvmPackages_latest.libclang.lib ];
+
   shellHook = ''
+    export PATH=$PATH:~/.cargo/bin
+    export PATH=$PATH:~/.rustup/toolchains/$RUSTC_VERSION-x86_64-unknown-linux-gnu/bin/
     echo "startup --output_base $(readlink -f ./bazel-output)" > "$(pwd)"/.output-bazelrc
   '';
 }
